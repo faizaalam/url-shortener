9c2ba40db861b387bcd6fff1f1fcba92540f426f
Glitch (surf-octave) <none>
2018-05-10 11:34:21 +0000

    🌎🐃 Checkpoint ./server.js:1075162/353

--- server.js
+++ server.js
@@ -5,6 +5,14 @@
 var express = require('express');
 var app = express();
 
+var result_json = {
+  'original_url': null,
+  'short_url': null
+};
+var error = {
+'error': 'Not a valid url'
+};
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -15,6 +23,15 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/new/:url', function(request,response){
+  result_json.original_url = request.params.url;
+
+    response.send(result_json);
+});
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
